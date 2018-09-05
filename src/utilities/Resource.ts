6741d41fac2779ef1bed0ec5e695e612c86fb219
kenkz447 <kenkz447@gmail.com>
2018-09-05 22:32:11 +0700

    fix Resource meta type

--- src/utilities/Resource.ts
+++ src/utilities/Resource.ts
@@ -2,12 +2,12 @@ import { RecordType } from './RecordTable';
 import { ResourceType } from './ResourceType';
 import { Store } from './Store';
 
-export interface ResourceProps<DataModel> {
+export interface ResourceProps<DataModel, Meta = {}> {
     resourceType: ResourceType;
     url: string;
     method: string;
     mapDataToStore?: (data: DataModel, resourceType: ResourceType, store: Store) => void;
-    afterFetch?: <TMeta>(params: ResourceParameter[], fetchResult: DataModel, meta?: TMeta) => void;
+    afterFetch?: (params: ResourceParameter[], fetchResult: DataModel, meta?: Meta) => void;
 }
 
 export interface ResourceParameter {
