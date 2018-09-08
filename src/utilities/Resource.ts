778a830faf79f0327b762f2f98fe90c4e8fc6a75
kenkz447 <kenkz447@gmail.com>
2018-09-08 15:20:20 +0700

    skip param.type === undefine

--- src/utilities/Resource.ts
+++ src/utilities/Resource.ts
@@ -36,7 +36,7 @@ export class Resource<DataModel, Meta = {}> {
         let uRL: string = this.url;
         const searchs: URLSearchParams = new URLSearchParams();
         for (const param of params) {
-            if (!param || param.type === 'body') {
+            if (!param || param.type === 'body' || param.value === undefined) {
                 continue;
             }
 
