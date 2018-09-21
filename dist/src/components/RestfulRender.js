d8802205daf30c75023cd3426d8e5f0eeb8b483b
kenkz447 <kenkz447@gmail.com>
2018-09-21 08:19:59 +0700

    build

--- dist/src/components/RestfulRender.js
+++ dist/src/components/RestfulRender.js
@@ -8,7 +8,7 @@ const utilities_1 = require("../utilities");
 class RestfulRender extends react_1.default.Component {
     constructor(props) {
         super(props);
-        this.state = Object.assign({}, props, { fetcher: this.props.fetcher || new utilities_1.Fetcher({ store: this.props.store }), fetching: false, componentRenderProps: {
+        this.state = Object.assign({}, props, { fetcher: this.props.fetcher || new utilities_1.Fetcher({ store: this.props.store }), fetching: true, componentRenderProps: {
                 data: null,
                 error: null
             } });
@@ -17,9 +17,6 @@ class RestfulRender extends react_1.default.Component {
         if (nextProps.resource !== prevState.resource ||
             nextProps.parameters !== prevState.parameters) {
             return Object.assign({}, nextProps, { fetcher: prevState.fetcher, componentRenderProps: prevState.componentRenderProps, needsUpdate: true, fetching: true });
-        }
-        if (nextProps.render !== prevState.render) {
-            return Object.assign({}, prevState, nextProps, { needsUpdate: true });
         }
         return null;
     }
