0c8b24c30358b70d1aee98614994cdf8ada0d971
kenkz447 <kenkz447@gmail.com>
2019-10-29 22:50:35 +0700

    feat: add urlToQueryParams utils

---
+++ src/utilities/__tests__/urlToQueryParams.test.ts
@@ -0,0 +1,44 @@
+import { urlToQueryParams } from '../urlToQueryParams';
+
+describe('urlToQueryParams', () => {
+    it('should work correctly with a url', () => {
+        const params = urlToQueryParams('https://random.url.com?Target=Offer&Method=findAll&filters%5Bhas_goals_enabled%5D%5BTRUE%5D=1&filters%5Bstatus%5D=active&fields%5B%5D=id&fields%5B%5D=name&fields%5B%5D=default_goal_name');
+
+        expect(params).toEqual([
+            {
+                parameter: 'Target',
+                type: 'query',
+                value: 'Offer',
+            },
+            {
+                parameter: 'Method',
+                type: 'query',
+                value: 'findAll',
+            },
+            {
+                parameter: 'fields',
+                type: 'query',
+                value: [
+                    'id',
+                    'name',
+                    'default_goal_name',
+                ],
+            },
+            {
+                parameter: 'filters',
+                type: 'query',
+                value: {
+                    'has_goals_enabled': {
+                        'TRUE': '1',
+                    },
+                    'status': 'active',
+                }
+            }
+        ]);
+    });
+
+    it('should return empty array with invalid url', () => {
+        const params = urlToQueryParams('');
+        expect(params).toEqual([]);
+    });
+});
---
+++ src/utilities/urlToQueryParams.tsx
@@ -0,0 +1,56 @@
+import { objectToQueryParams } from './objectToQueryParams';
+
+const getParamsAsObject = (url: string) => {
+    url = url.substring(url.indexOf('?') + 1);
+
+    var re = /([^&=]+)=?([^&]*)/g;
+    var decodeRE = /\+/g;
+
+    var decode = (str: string) => {
+        return decodeURIComponent(str.replace(decodeRE, ' '));
+    };
+
+    var params = {}, e;
+    while (e = re.exec(url)) {
+        var k = decode(e[1]), v = decode(e[2]);
+        if (k.substring(k.length - 2) === '[]') {
+            k = k.substring(0, k.length - 2);
+            (params[k] || (params[k] = [])).push(v);
+        } else { params[k] = v; }
+    }
+
+    var assign = (obj: {}, keyPath: string[], value: unknown) => {
+        var lastKeyIndex = keyPath.length - 1;
+        for (var i = 0; i < lastKeyIndex; ++i) {
+            var key = keyPath[i];
+            if (!(key in obj)) {
+                obj[key] = {};
+            }
+            obj = obj[key];
+        }
+        obj[keyPath[lastKeyIndex]] = value;
+    };
+
+    const paramsKeys = Object.keys(params);
+    for (const prop of paramsKeys) {
+        var structure = prop.split('[');
+        if (structure.length > 1) {
+            const levels: string[] = [];
+
+            structure.forEach((item, i) => {
+                var key = item.replace(/[?[\]\\ ]/g, '');
+                levels.push(key);
+            });
+
+            assign(params, levels, params[prop]);
+            delete (params[prop]);
+        }
+    }
+
+    return params;
+};
+
+export const urlToQueryParams = (url: string) => {
+    const obj = getParamsAsObject(url);
+    return objectToQueryParams(obj);
+};
